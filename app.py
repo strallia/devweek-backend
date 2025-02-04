86e5f45a58f492e2f1b0ab14c79fdf07c0c71f3b
Aash2101 <amachira@asu.edu>
2025-02-04 11:15:25 -0700

    Initial commit- Setup Flask app with PostgreSQL and Flask-Migrate

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'default_secret_key')
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+
 @app.route('/')
-def home():
-    return "Hello!"
+def index():
+    return "Hello, Flask with PostgreSQL!"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
